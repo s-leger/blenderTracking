5020446d5beb8583c83b831280690bb975f11509
s-leger <stephen@3dservices.ch>
2017-02-11 00:43:12 +0100

    Local error evaluation
    Instead of weighting according average error, this release does evaluate reprojection error for each track on every frame and animate track weight according.
    Smooth transition of incomplete tracks by weighting entry and out points. 
    
    Note: 
    The reprojection error evaluation dosen't take account of lens distortions.

--- space_clipEditor_refine_solution.py
+++ space_clipEditor_refine_solution.py
@@ -21,10 +21,10 @@ bl_info = {
     "name": "Refine tracking solution",
     "author": "Stephen Leger",
     "license": "GPL",
-    "version": (1, 0, 0),
+    "version": (1, 1, 0),
     "blender": (2, 7, 8),
     "location": "Clip Editor > Tools > Solve > Refine Solution",
-    "description": "Refine motion solution by setting track weight according average error",
+    "description": "Refine motion solution by setting track weight according reprojection error",
     "warning": "",
     "wiki_url": "https://github.com/s-leger/blenderTracking/wiki",
     "tracker_url": "https://github.com/s-leger/blenderTracking/issues",
@@ -34,6 +34,7 @@ bl_info = {
 
 import bpy
 import math
+from mathutils import Vector
 
 class OP_Tracking_refine_solution(bpy.types.Operator):
     """Set track weight by error and solve camera motion"""
@@ -41,23 +42,75 @@ class OP_Tracking_refine_solution(bpy.types.Operator):
     bl_label = "Refine"
     bl_options = {"UNDO"}
     
-    error = bpy.props.FloatProperty(
-        name="Target error", 
-        description="Refine motion track target error", 
-        default=0.3, 
-        min=0.01)
-    
     @classmethod
     def poll(cls, context):
         return (context.area.spaces.active.clip is not None)
         
     def execute(self, context):
+        error = context.window_manager.TrackingTargetError
+        smooth = context.window_manager.TrackingSmooth
         clip = context.area.spaces.active.clip
-        for track in clip.tracking.tracks:
-            if track.average_error == 0:
-                track.weight = 0
+        try:
+            tracking = clip.tracking
+            tracks = tracking.tracks
+            winx = float(clip.size[0])
+            winy = float(clip.size[1])
+            aspy =  1.0 / tracking.camera.pixel_aspect
+            start = tracking.reconstruction.cameras[0].frame
+            end   = tracking.reconstruction.cameras[-1].frame
+        except:
+            return {'CANCELED'}
+        
+        marker_position = Vector()
+        
+        for frame in range(start, end):
+            camera = tracking.reconstruction.cameras.find_frame(frame)
+            if camera is not None:
+                imat = camera.matrix.inverted()
+                projection_matrix = imat.transposed()
             else:
-                track.weight = self.error/(track.average_error/track.weight)
+                continue
+            
+            for track in tracking.tracks:
+                marker = track.markers.find_frame(frame)
+                if marker is None:
+                    continue
+                    
+                # weight incomplete tracks on start and end
+                if frame > start + smooth and frame < end - smooth:
+                    for m in track.markers:
+                        if not m.mute:
+                            tstart = m
+                            break
+                    for m in reversed(track.markers):
+                        if not m.mute:
+                            tend = m
+                            break
+                    dt = min(0.5 * (tend.frame - tstart.frame), smooth)
+                    if dt > 0:
+                        t0 = min(1.0, (frame - tstart.frame) / dt)
+                        t1 = min(1.0, (tend.frame - frame) / dt)
+                        tw = min(t0, t1)
+                    else:
+                        tw = 0.0
+                else:
+                    tw = 1.0
+                    
+                reprojected_position = track.bundle * projection_matrix
+                reprojected_position = reprojected_position / -reprojected_position.z * tracking.camera.focal_length_pixels
+                reprojected_position = Vector((tracking.camera.principal[0] + reprojected_position[0],tracking.camera.principal[1] * aspy + reprojected_position[1], 0))
+                
+                marker_position[0] = (marker.co[0] + track.offset[0]) * winx
+                marker_position[1] = (marker.co[1] + track.offset[1]) * winy * aspy
+                
+                dp = marker_position - reprojected_position
+                if dp.length == 0:
+                    track.weight = 1.0
+                else:
+                    track.weight = min(1.0, tw * error / dp.length)
+                track.keyframe_insert("weight", frame=frame)
+            
+            
         bpy.ops.clip.solve_camera()
         return{'FINISHED'}
         
@@ -73,8 +126,25 @@ class OP_Tracking_reset_solution(bpy.types.Operator):
     
     def execute(self, context):
         clip = context.area.spaces.active.clip
-        for track in clip.tracking.tracks:
-            track.weight = 1.0
+        try:
+            tracking = clip.tracking
+            tracks = tracking.tracks
+            start = tracking.reconstruction.cameras[0].frame
+            end   = tracking.reconstruction.cameras[-1].frame
+        except:
+            return {'CANCELED'}
+        start = tracking.reconstruction.cameras[0].frame
+        end   = tracking.reconstruction.cameras[-1].frame
+        for frame in range(start, end):
+            camera = tracking.reconstruction.cameras.find_frame(frame)
+            if camera is None:
+                continue
+            for track in tracking.tracks:
+                marker = track.markers.find_frame(frame)
+                if marker is None:
+                    continue
+                track.weight = 1.0
+                track.keyframe_insert("weight", frame=frame)       
         bpy.ops.clip.solve_camera()
         return{'FINISHED'}
 
@@ -96,7 +166,9 @@ class RefineMotionTrackingPanel(bpy.types.Panel):
         row = box.row(align=True)
         row.prop(context.window_manager, "TrackingTargetError", text="Target error")
         row = box.row(align=True)
-        row.operator("tracking.refine_solution").error = context.window_manager.TrackingTargetError
+        row.prop(context.window_manager, "TrackingSmooth", text="Smooth transition")
+        row = box.row(align=True)
+        row.operator("tracking.refine_solution")
         row.operator("tracking.reset_solution")
   
 def register():
@@ -105,6 +177,11 @@ def register():
         description="Refine motion track target error", 
         default=0.3, 
         min=0.01)
+    bpy.types.WindowManager.TrackingSmooth = bpy.props.FloatProperty(
+        name="Smooth transition", 
+        description="Smooth weight transition on start and end of incomplete tracks", 
+        default=25, 
+        min=1)
     bpy.utils.register_module(__name__)        
         
 def unregister():
@@ -113,3 +190,5 @@ def unregister():
     
 if __name__ == "__main__":
     register()
+    
+    
